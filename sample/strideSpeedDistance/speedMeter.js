081801e47192d78b5749a5d326ccb23a25cdb391
Johannes <johnsn@johannes-HP-EliteBook-840-G5>
2023-12-14 20:49:32 +0100

    Update speedMeter.js simulating pace values

--- sample/strideSpeedDistance/speedMeter.js
+++ sample/strideSpeedDistance/speedMeter.js
@@ -1,43 +1,36 @@
 const DEBUG = false;
 
 const speedMeter = function (SensorPin){
-    const gpio = require('rpi-gpio');
+    // const gpio = require('rpi-gpio');
     
     const treadmillLength = 293;   //cm
     
     let watchDog = 0;
     let timePrev;
-    let speed = 0;
+    let speed = 10;
+    let increment = 1;
     let prevSpeed = 0;
 
-    gpio.setup(7, gpio.DIR_IN, gpio.EDGE_FALLING);	// Pin used on raspberry pi for sensor
+    // gpio.setup(7, gpio.DIR_IN, gpio.EDGE_FALLING);	// Pin used on raspberry pi for sensor
 
-    gpio.on('change', function (channel, value) {
+    /*gpio.on('change', function (channel, value) {
         let timeDelta = Date.now() - timePrev;
         speed = ((((1000 / timeDelta) * 3600) * treadmillLength) / 100000).toFixed(1);
         timePrev = Date.now();
         watchDog = 0;
-    });
+    });*/
 
     this.getSpeed = function () {
-        if (watchDog === 0){
-            watchDog = 2;
-        } else if ( watchDog >= 2 && watchDog <= 20) {
-            watchDog++;
-            speed = prevSpeed;
-        } else if (watchDog > 20 && watchDog < 30) {
-            speed = (speed / 2).toFixed(1);
-	    watchDog++;
-        } else if (watchDog > 29) {
-            speed = 0;
+
+        if (speed > 20) {
+            increment = -1;
         }
-        if (speed > 20){
-            speed = prevSpeed;
+        if (speed < 10) {
+            increment = 1;
         }
-        if (DEBUG){
-            console.log("PrevSpeed: %s Speed: %s Watchdog: %s", prevSpeed, speed, watchDog);
-        }
-        prevSpeed = speed;
+
+        speed = speed + increment;
+
         return speed;
     }
 };
